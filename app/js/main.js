5b24978ee364b4a72d661c49c5e290ddd5c9edca
erlan <erlannasyrov9@gmail.com>
2023-04-26 22:27:22 +0600

    finish header

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,14 @@ $(function () {
     $('.header__nav-list').toggleClass('header__nav-list--active');
     $('.header').toggleClass('header--active');
   })
+
+  $('.header__button').on('click', function () {
+    $('.login-form').addClass('login-form--active');
+    $('.header').addClass('header--modify');
+  })
+
+  $('.login-form__closer-btn').on('click', function () {
+    $('.login-form').removeClass('login-form--active');
+    $('.header').removeClass('header--modify');
+  })
 });
