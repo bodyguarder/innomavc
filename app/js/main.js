8d57e2daf604d1a38659d0873c6bee626b63a565
erlan <erlannasyrov9@gmail.com>
2023-05-02 21:07:51 +0600

    services-section finish

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,18 @@ $(function () {
     $('.menu-btn__burger').toggleClass('menu-btn__burger--active');
     $('.header__nav-list').toggleClass('header__nav-list--active');
     $('.header').toggleClass('header--active');
+    $('body').toggleClass('hidden');
   })
 
   $('.header__button').on('click', function () {
     $('.login-form').addClass('login-form--active');
     $('.header').addClass('header--modify');
+    $('body').addClass('hidden');
   })
 
   $('.login-form__closer-btn').on('click', function () {
     $('.login-form').removeClass('login-form--active');
     $('.header').removeClass('header--modify');
+    $('body').removeClass('hidden');
   })
 });
